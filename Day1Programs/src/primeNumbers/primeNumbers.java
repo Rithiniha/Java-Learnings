1426fdfd7c06f77537dc1dfc79a7b0f9f790524d
Rithiniha kamalanathan <164982399+Rithiniha@users.noreply.github.com>
2025-04-04 22:16:19 +0530

    Update primeNumbers.java

--- Day1Programs/src/primeNumbers/primeNumbers.java
+++ Day1Programs/src/primeNumbers/primeNumbers.java
@@ -5,7 +5,7 @@ public class primeNumbers {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the number : ");
+		System.out.println("Enter The Number : ");
 		int  n = scanner.nextInt();
 		
 		for (int i=2;i<=n;i++) {
